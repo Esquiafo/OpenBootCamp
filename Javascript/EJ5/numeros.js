7879b1ac50616e49370c70a8221ce0d0e25f10dc
Esquiafo <57331052+Esquiafo@users.noreply.github.com>
2022-06-17 01:00:55 -0300

    EJ5

---
+++ Javascript/EJ5/numeros.js
@@ -0,0 +1,8 @@
+const hightCm = 170
+const hightM = 1.70
+const pesoKg = 74.1
+
+const hightCeil = Math.ceil(hightM)
+const pesoFloor = Math.floor(pesoKg)
+
+const equeality = Number.MAX_VALUE + 1 === Number.MAX_VALUE
