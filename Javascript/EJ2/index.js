c855c84a3a294c48306d25f912ddbbe811438013
Esquiafo <57331052+Esquiafo@users.noreply.github.com>
2022-06-16 21:38:25 -0300

    EJ2

---
+++ Javascript/EJ2/index.js
@@ -0,0 +1,6 @@
+const list = ["Leonardo Capone", 24, true,  new Date(1997, 09, 22),{
+    titulo: "Wolfheart",
+    autor: "Richard A. Knaak",
+    fecha:  new Date(2011, 09, 13),
+    url: 'https://wowpedia.fandom.com/wiki/Wolfheart',
+}]
