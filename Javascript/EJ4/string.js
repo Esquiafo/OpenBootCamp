302aa06cfcd384303ba679889960b132d47d4b2c
Esquiafo <57331052+Esquiafo@users.noreply.github.com>
2022-06-17 00:06:25 -0300

    EJ4

---
+++ Javascript/EJ4/string.js
@@ -0,0 +1,11 @@
+let nombre = "Leonardo"
+let apellido = "Capone"
+
+let estudiante = nombre + " " + apellido
+let estudianteMayus = estudiante.toUpperCase()
+let estudianteMinus = estudiante.toLowerCase()
+let estudiandteNum = estudiante.length
+let firstLetter = nombre.charAt(0)
+let lastLetter = nombre.charAt(nombre.length - 1)
+let estudianteReplace = estudiante.replace(' ','')
+let estudianteBool = estudiante.includes(nombre)
